72df9ccd50bb5d02e993b5ee55170ee77e81775b
LightShade12 <subhamswostikpradhan2004@gmail.com>
2023-07-12 11:30:05 +0530

    cleaned client cpp

--- MiniRay/src/creator/client.cpp
+++ MiniRay/src/creator/client.cpp
@@ -15,58 +15,6 @@
 
 //to be implemented by client
 
-class DockspaceLayer : public Layer
-{
-public:
-
-	virtual void OnUIRender() override
-	{
-		ImGui::BeginMainMenuBar();
-		ImGui::MenuItem("File");
-		ImGui::MenuItem("Edit");
-		ImGui::MenuItem("View");
-		ImGui::MenuItem("Window");
-		ImGui::MenuItem("Help");
-		ImGui::InputTextWithHint("", "search actions", m_str_buffer, IM_ARRAYSIZE(m_str_buffer));
-		ImGui::EndMainMenuBar();
-		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport(), m_dockflags);
-		if (ImGui::BeginViewportSideBar("##MainStatusBar", NULL, ImGuiDir_Down, ImGui::GetFrameHeight(), m_windowflags)) {
-			if (ImGui::BeginMenuBar()) {
-				ImGui::Text("Status");
-				ImGui::EndMenuBar();
-			}
-		};
-	};
-	char m_str_buffer[128] = "";
-	ImGuiDockNodeFlags m_dockflags = ImGuiDockNodeFlags_PassthruCentralNode;
-	ImGuiWindowFlags m_windowflags = ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
-};
-
-class SceneGraphLayer : public Layer
-{
-public:
-
-	virtual void OnUIRender() override
-	{
-		ImGui::Begin("Scene Graph");
-		ImGui::InputTextWithHint("", "start typing to search", m_str_buffer, IM_ARRAYSIZE(m_str_buffer));
-		ImGui::End();
-	}
-	char m_str_buffer[128] = "";
-};
-
-class NodeEditorLayer : public Layer
-{
-public:
-
-	virtual void OnUIRender() override
-	{
-		ImGui::Begin("Editor");
-		//ImGui::Button("Button");
-		ImGui::End();
-	}
-};
-
 application* create_application(int argc, char* argv[]) {
 	std::cout << "Running from: " << argv[0] << "\n";
 	application_specification spec;
@@ -74,12 +22,7 @@ application* create_application(int argc, char* argv[]) {
 	spec.iconpath = "resources/vgui/appicon.png";
 	auto app = new application(spec);
 
-	//app->PushLayer<DockspaceLayer>();
 	app->PushLayer<EditorLayer>();
-	//app->PushLayer<NodeInspectorLayer>();
-	//app->PushLayer<SceneGraphLayer>();
-	//app->PushLayer<NodeEditorLayer>();
-	//app->PushLayer<AttributeManagerLayer>();
 	app->PushLayer<DevWindowLayer>();
 
 	return app;
